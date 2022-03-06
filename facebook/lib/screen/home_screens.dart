e9607a4ef3c13e3c14cadf41b6460e8fef2f8bf6
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-07 00:38:41 +0700

    Create home_screens.dart

---
+++ facebook/lib/screen/home_screens.dart
@@ -0,0 +1,29 @@
+import 'package:facebook/config/palette.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            title: Text(
+              'facebook',
+              style: TextStyle(
+                color: Palette.facebookBlue,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
