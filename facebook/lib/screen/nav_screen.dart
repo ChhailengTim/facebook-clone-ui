96c96a02b442090c0c0073272b11b38d087b0625
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-09 11:40:14 +0700

    Create nav_screen.dart

---
+++ facebook/lib/screen/nav_screen.dart
@@ -0,0 +1,52 @@
+import 'package:facebook/screen/home_screens.dart';
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+import '../widgets/custom_tab_bar.dart';
+
+class NavScreen extends StatefulWidget {
+  const NavScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NavScreen> createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
+  final List<Widget> _screens = [
+    HomeScreen(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+  ];
+  final List<IconData> _icons = const [
+    Icons.home,
+    Icons.ondemand_video,
+    MdiIcons.accountCircleOutline,
+    MdiIcons.facebookGaming,
+    MdiIcons.bellOutline,
+    Icons.menu,
+  ];
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: _icons.length,
+      child: Scaffold(
+        body: TabBarView(
+          physics: NeverScrollableScrollPhysics(),
+          children: _screens,
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.only(bottom: 12.0),
+          child: CustomTabBar(
+            icons: _icons,
+            selectedIndex: _selectedIndex,
+            onTap: (index) => setState(() => _selectedIndex = index),
+          ),
+        ),
+      ),
+    );
+  }
+}
